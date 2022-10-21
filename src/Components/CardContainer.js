fbb256bd1db673dd0c0999f59022af4ee526fcde
JoshG <joshcode25@gmail.com>
2022-10-20 22:11:29 -0500

    updated image src to site URL

--- src/Components/CardContainer.js
+++ src/Components/CardContainer.js
@@ -3,13 +3,14 @@ import '../CSS/cardContainer.css'
 
 const CardContainer = ({cardInfo}) => {
     const cardName = (cardInfo) ? cardInfo[0].name : 'default';
+    const cardNameDash = cardName.replace(/[.,\/#!$%\^&\*;:{}=\_`'~()]/g,"").split(" ").join("-");
     const toggleInfo = (cardName) => (console.log(cardName));
 
     return (
         <div onClick={() => {toggleInfo(cardName)}} className='cardContainer'>
             <img 
                 className='cardImg'
-                src={`${process.env.PUBLIC_URL}/Artwork/${cardName}.jpg`} 
+                src={`https://calebgannon.com/wp-content/uploads/cardsearch-images/${cardNameDash}.jpg`} 
                 alt={`${cardName}`}
             />
         </div>
